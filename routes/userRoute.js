e358828be9730ec8891238bded19316a52f9c63e
mkrandhawa71@gmail.com <mkrandhawa71@gmail.com>
2024-06-25 15:37:59 +0100

    Added login route

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,6 +3,12 @@ const userController = require('../controllers/userController');
 
 const router = express.Router();
 
+
+
+router.post('/login', userController.login);
+
 router.post('/signup', userController.registerUser);
+
+
 
 module.exports = router;
