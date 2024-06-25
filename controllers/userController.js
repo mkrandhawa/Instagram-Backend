5102ba53732d10a2a5789e3bff0555c7c98fb5b7
mkrandhawa71@gmail.com <mkrandhawa71@gmail.com>
2024-06-25 15:37:36 +0100

    Added login functionalit

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,7 @@ const mongoose= require('mongoose');
 const User = require('../models/userModel');
 
 
+// REGISTER THE USER
 exports.registerUser = async(req, res, next)=>{
 
     const userData = req.body.user
@@ -24,4 +25,45 @@ exports.registerUser = async(req, res, next)=>{
             user
         }
     })
+}
+
+// LOGIN THE USER
+exports.login = async(req, res, next)=>{
+
+    console.log(req.body)
+ 
+
+    const {username, password} = req.body.user;
+    console.log(username)
+
+    //Check if the username or password fields are available
+    if (!username || !password){
+        return next(
+            res.status(400).json({
+                status: 'error',
+                message: 'Missing username or password'
+            })
+        )
+    }
+    
+    const user = await User.findOne({emailPhone: username}).select("+password");
+
+    console.log('This is user',user)
+
+    //Check if there is any user with that data
+    if (!user || !(password === user.password)){
+        return next ( 
+            res.status(401).json({
+                status:'error',
+                message: 'Wrong email/username or password'
+            })
+        )
+    }
+    else{
+        console.log('Logged in successfully')
+        res.status(200).json({
+            status: 'success',
+        })
+        
+    }
 }
