25c3a9e32722768beefd217b9f40fa9b72d670c7
mkrandhawa71@gmail.com <mkrandhawa71@gmail.com>
2024-06-25 13:51:12 +0100

    Added user route, cors module

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const mongoose = require("mongoose");
 const express = require("express");
+const userRoute = require('./routes/userRoute');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+app.use('/api/v1/users', userRoute);
 
 module.exports = app;
