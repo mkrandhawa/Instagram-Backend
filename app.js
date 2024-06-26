f3aecf3129e00d006dd85532f77a186e502f4f77
mkrandhawa71@gmail.com <mkrandhawa71@gmail.com>
2024-06-26 19:34:48 +0100

    Added cors module

--- app.js
+++ app.js
@@ -2,10 +2,20 @@ const mongoose = require("mongoose");
 const express = require("express");
 const userRoute = require('./routes/userRoute');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 
 const app = express();
-app.use(cors());
+// app.use(cors());
+
+app.use(cors({
+    origin: 'http://localhost:3000', // Replace with your frontend origin
+    credentials: true // Allow cookies to be sent cross-origin
+}));
+
+
 app.use(express.json());
+app.use(cookieParser());
+
 
 
 app.use('/api/v1/users', userRoute);
