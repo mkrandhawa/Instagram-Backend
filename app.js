2e9a03b8ba4d459fd4f2aa56f366ec357de30f71
mkrandhawa <mkrandhawa71@gmail.com>
2024-07-06 17:23:11 +0100

    Add path to public files

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ const express = require("express");
 const userRoute = require('./routes/userRoute');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const path = require("path");
 
 const app = express();
 // app.use(cors());
 
+//SERVING STATIC FILES accessing the static files
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use(cors({
-    origin: 'http://localhost:3000', // Replace with your frontend origin
+    origin: 'http://localhost:3000', 
     credentials: true // Allow cookies to be sent cross-origin
 }));
 
