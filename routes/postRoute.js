38edd715c33ecca64aeb2ccb587688aa47161695
mkrandhawa <mkrandhawa71@gmail.com>
2024-12-09 22:57:34 +0000

    Added comment and story

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -5,15 +5,23 @@ const userController = require('../controllers/userController');
 
 const router = express.Router();
 
+
 router.get('/',  userController.protect, postController.getAll);
 
 router.get('/:id', userController.protect, postController.getPost);
 
+router.get('/:id/comments', userController.protect, postController.getAllCommentsOnPost);
+
 router.post('/uploadPost',  userController.protect, postController.uploadPost);
+
+router.post('/:id/comment', userController.protect, postController.addComment);
 
 router.patch('/:id/saveUnsavePost', userController.protect, postController.saveUnsavePost);
 
 router.patch('/:id/likeUnlikePost', userController.protect, postController.likeUnlikePost);
+
+router.delete('/:id/comment/:commentId', userController.protect, postController.deleteComment);
+
 
 
 
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -10,11 +10,17 @@ router.get('/all-users', userController.protect, userController.getAll);
 
 router.get('/savedPosts', userController.protect, userController.getSavedPosts);
 
+router.get('/getAllStories', userController.protect, userController.getAllStories);
+
 router.patch('/:id/addRemoveFollower', userController.protect, userController.addRemoveFollower);
 
 router.post('/login', userController.login);
 
 router.post('/signup', userController.registerUser);
+
+router.post('/uploadStory', userController.protect, userController.uploadStory);
+
+router.delete('/:id/delete', userController.protect, userController.deleteStory);
 
 
 
